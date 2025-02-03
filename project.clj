55cbae9ae72b1504f53e87f251b5555c658203b7
sdedovic <stevan@dedovic.com>
2025-02-02 20:19:45 -0600

    update for personal deployment (sdedovic)

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject lein-modules "0.3.12-SNAPSHOT"
+(defproject com.dedovic/lein-modules-new-profiles "0.3.12-SNAPSHOT"
   :description "Similar to Maven multi-module projects, but less sucky"
-  :url "https://github.com/jcrossley3/lein-modules"
+  :url "https://github.com/sdedovic/lein-modules-new-profiles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :aliases {"all" ["do" "clean," "test," "install"]}
   :signing {:gpg-key "92439EF5"}
   :plugins [[lein-file-replace "0.1.0"]]
-  :deploy-repositories {"releases" :clojars}
+  :deploy-repositories  [["releases" {:sign-releases false
+                                      :url           "https://clojars.org/repo"
+                                      :username      :env/clojars_user
+                                      :password      :env/clojars_token}]]
   :release-tasks
   [["vcs" "assert-committed"]
    ["change" "version" "leiningen.release/bump-version" "release"]
@@ -15,7 +18,7 @@
    ["file-replace" "README.md" "lein-modules \"" "\"]" "version"]
 
    ["vcs" "commit"]
-   ["vcs" "tag"]
+   ["vcs" "tag" "--no-sign"]
    ["deploy"]
    ["change" "version" "leiningen.release/bump-version"]
    ["vcs" "commit"]
